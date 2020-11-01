6e91c672a7aa4521a59c8f65a837d3c50a4e13b8
Nataliia Krikun <58227698+Natika888@users.noreply.github.com>
2020-11-01 12:39:12 +0200

    feat: add TA and WS

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  const conditions = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  let res = [];
+
+  if (!Array.isArray(arr)) {
+    throw new Error('Not implemented');
+  }
+
+  for ( let i = 0; i< arr.length; i++) {
+    if (!conditions.includes(arr[i])) {
+      res.push(arr[i]);
+    } else {
+      switch (arr[i]) {
+        case '--discard-prev': 
+          if (i-2 > 0 && arr[i-2] === '--discard-next') break;
+             else if (i-1 >0 || i-1 === 0) {res.pop();}
+          break;
+
+        case '--discard-next':   
+          if (i+1 < arr.length) {i++}
+          break;
+
+        case '--double-next': 
+          if (i+1 < arr.length) {res.push(arr[i+1]);}
+            else {i++}
+          break;
+
+        case '--double-prev': 
+          if (i-2 > 0 && arr[i-2] === '--discard-next') break;
+            else if (i-1 >0 || i-1 === 0) {res.push(arr[i-1]);}
+            else {i++}
+          break;
+
+      }
+    }
+  }
+  return res;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  switch (date) {
+    case 'winter': season = 'winter';
+      break;
+      case 'spring': season = 'spring';
+      break;
+      case 'summer': season = 'summer';
+      break;
+      case 'fall': season = 'fall';
+      break;
+  }
+  let month = date.getMonth();
+   let season = ''; 
+
+   if (arguments.length === 0) {season ='Unable to determine the time of year!'}
+   else if (typeof month !== 'number') {throw new CustomError('argument is invalid');}
+
+    switch (month) {
+      case 2:
+      case 3:
+      case 4: season = 'spring';
+      break;
+      case 5:
+      case 6:
+      case 7: season = 'summer';
+      break;
+      case 8:
+      case 9:
+      case 10: season = 'fall';
+      break;
+      case 11:
+      case 0:
+      case 1: season = 'winter';
+      break;
+    }     
 };
